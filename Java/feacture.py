8fd3ddbc439290175d9e98ccb162b99e3f7a1ed8
Cola <100990149+colaloczy@users.noreply.github.com>
2026-03-29 18:27:34 +0800

    Refactor feature extraction and LDA similarity functions
    Refactor feature extraction code and improve LDA similarity calculation.

--- Java/feacture.py
+++ Java/feacture.py
@@ -1,274 +1,128 @@
+import os
+import time
 import javalang
-import Levenshtein
 import pandas as pd
 import multiprocessing as mp
-import tqdm
-import math
-from functools import partial
-import time
-from sklearn.decomposition import LatentDirichletAllocation
+from functools import lru_cache
+
+import Levenshtein
+import numpy as np
+
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.metrics.pairwise import cosine_similarity
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
 
-# The dataset is under the master branch, decompress id2sourcecode into the dataset folder.
-def lda_similarity(group1, group2, num_topics=5):
-    """计算两个字符串列表的LDA主题相似性"""
-    
-    # 确保输入为文本列表，并且都是字符串
-    if not isinstance(group1, list) or not isinstance(group2, list):
-        raise ValueError("group1 和 group2 必须是字符串列表。")
-    if not all(isinstance(item, str) for item in group1):
-        raise ValueError("group1 中的所有元素必须是字符串。")
-    if not all(isinstance(item, str) for item in group2):
-        raise ValueError("group2 中的所有元素必须是字符串。")
-    
-    # 将两个组拼接为一份文档
-    documents = [' '.join(group1), ' '.join(group2)]
-    
-    # 使用CountVectorizer转换文档为词袋模型，去掉停用词
-    stop_words = stopwords.words('english')
-    vectorizer = CountVectorizer(stop_words=stop_words)
-    doc_term_matrix = vectorizer.fit_transform(documents)
-    
-    # 训练LDA模型
-    lda_model = LatentDirichletAllocation(n_components=num_topics, random_state=0)
-    lda_model.fit(doc_term_matrix)
-    
-    # 计算两个文档的主题分布
-    topic_dist_1 = lda_model.transform(doc_term_matrix[0:1])[0]
-    topic_dist_2 = lda_model.transform(doc_term_matrix[1:2])[0]
-    
-    # 计算余弦相似度
-    similarity = cosine_similarity([topic_dist_1], [topic_dist_2])[0][0]
-    
-    return similarity
+@lru_cache(maxsize=None)
+def get_java_tokens(file_path):
+    tokens = []
+    try:
+        with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+            code = f.read()
+
+        for tok in javalang.tokenizer.tokenize(code):
+            if isinstance(tok, javalang.tokenizer.Identifier):
+                tokens.append("id")
+            else:
+                tokens.append(tok.value)
+
+    except Exception:
+        pass
+
+    return tokens
+
+def jaccard_sim(a, b):
+    sa, sb = set(a), set(b)
+    return len(sa & sb) / (len(sa | sb) + 1e-6)
 
 
-
-global logfile  # 声明logfile为全局变量
-# 全局变量
-logfile = None
-
-def get_sim(tool, dataframe):
-    inputpath = 'dataset/id2sourcecode/'
-
-    sim = []
-    for _, pair in dataframe.iterrows():
-        id1, id2 = pair.FunID1, pair.FunID2
-
-        sourcefile1 = inputpath + str(id1) + '.java'
-        sourcefile2 = inputpath + str(id2) + '.java'
-        try:
-            similarity = runner(tool, sourcefile1, sourcefile2)
-        except Exception as e:
-            similarity = repr(e).split('(')[0]
-            log = "\n" + time.asctime() + "\t" + tool + "\t" + str(id1) + "\t" + str(id2) + "\t" + similarity
-            similarity = 'False'
-#         print(similarity)
-        sim.append(similarity)
-
-    return sim
+def jaro_sim(a, b):
+    return Levenshtein.jaro(" ".join(a), " ".join(b))
 
 
-def getCodeBlock(file_path):     
-    block = []
-    # print(file_path)
-    with open(file_path, 'r', encoding='utf-8') as temp_file:
-        lines = temp_file.readlines()
-        for line in lines:
-            tokens = list(javalang.tokenizer.tokenize(line))
-            for token in tokens:
-                if type(token) == javalang.tokenizer.Identifier:
-                    block.append("id")
-                else:
-                    block.append(token.value)
-    return block
+def levenshtein_ratio(a, b):
+    return Levenshtein.ratio(" ".join(a), " ".join(b))
 
-def getCodeBlock_type(file_path):     # 类型
-    block = []
-    # print(file_path)
-    with open(file_path, 'r', encoding='utf-8') as temp_file:
-        lines = temp_file.readlines()
-        for line in lines:
-            tokens = list(javalang.tokenizer.tokenize(line))
-            for token in tokens:
-                token_type = str(type(token))[:-2].split(".")[-1]
-                block.append(token_type)
-    return block
+def build_global_lda(all_files, n_topics=5):
+    docs = []
+    index = {}
 
-def getCodeBlock_token_and_type(file_path):     # 类型加token
-    block = []
-    # print(file_path)
-    with open(file_path, 'r', encoding='utf-8') as temp_file:
-        lines = temp_file.readlines()
-        for line in lines:
-            tokens = list(javalang.tokenizer.tokenize(line))
-            for token in tokens:
-                if type(token) == javalang.tokenizer.Identifier:
-                    block.append("id")
-                else:
-                    block.append(token.value)
-                token_type = str(type(token))[:-2].split(".")[-1]
-                block.append(token_type)
-    return block
+    for i, f in enumerate(all_files):
+        tokens = get_java_tokens(f)
+        docs.append(" ".join(tokens))
+        index[f] = i
 
-def runner(tool, sourcefile1, sourcefile2):
-    block1 = getCodeBlock(sourcefile1)
-    block2 = getCodeBlock(sourcefile2)
-    if tool == 't1':
-        return Jaccard_sim(block1, block2)
-    elif tool == 't3':
-        return Jaro_sim(block1, block2)
-    elif tool == 't6':
-        return Levenshtein_ratio(block1, block2)
-    elif tool == 't11':
-        return lda_similarity(block1, block2)  # 通过LDA计算相似性
+    vectorizer = CountVectorizer(min_df=2)
+    X = vectorizer.fit_transform(docs)
 
+    lda = LatentDirichletAllocation(
+        n_components=n_topics,
+        random_state=42
+    )
+    topic_matrix = lda.fit_transform(X)
 
-def intersection_and_union(group1, group2):
-    intersection = 0
-    union = 0
-    triads_num1 = {}
-    triads_num2 = {}
-    for triad1 in group1:
-        triads_num1[triad1] = triads_num1.get(triad1, 0) + 1
-    for triad2 in group2:
-        triads_num2[triad2] = triads_num2.get(triad2, 0) + 1
+    return topic_matrix, index
 
-    for triad in list(set(group1).union(set(group2))):
-        intersection += min(triads_num1.get(triad, 0), triads_num2.get(triad, 0))
-        union += max(triads_num1.get(triad, 0), triads_num2.get(triad, 0))
-    return intersection, union
+def compute_pair(args):
+    f1, f2, label, topic_matrix, index = args
 
+    t1 = get_java_tokens(f1)
+    t2 = get_java_tokens(f2)
 
-def Jaccard_sim(group1, group2):
-    # Jaccard 系数
+    jac = jaccard_sim(t1, t2)
+    # jaro = jaro_sim(t1, t2)
+    lev = levenshtein_ratio(t1, t2)
 
-    intersection, union = intersection_and_union(group1, group2)
-    # 除零处理
-    sim = float(intersection) / union if union != 0 else 0
-    return sim
+    v1 = topic_matrix[index[f1]]
+    v2 = topic_matrix[index[f2]]
+    lda_sim = cosine_similarity([v1], [v2])[0][0]
 
+    len_ratio = len(t1) / (len(t2) + 1e-6)
+    # rename_ratio = 1 - jac
 
-
-def Jaro_sim(group1, group2):
-    # Jaro相似性
-
-    sim = Levenshtein.jaro(group1, group2)
-    return sim
-
-
-def Levenshtein_ratio(group1, group2):
-    # Levenshtein比
-    sim = Levenshtein.ratio(group1, group2)
-    return sim
-
-def cut_df(df, n):
-    df_num = len(df)
-    every_epoch_num = math.floor((df_num/n))
-    df_split = []
-    for index in range(n):
-        if index < n-1:
-            df_tem = df[every_epoch_num * index: every_epoch_num * (index + 1)]
-        else:
-            df_tem = df[every_epoch_num * index:]
-        df_split.append(df_tem)
-    return df_split
-
+    return [
+        jac,lev, lda_sim,
+        len_ratio,
+        label
+    ]
 
 def main():
     inputcsv = "dataset/nonclone.csv"
-    #inputcsv = "dataset/clone.csv"
-#     inputcsv = "dataset/type-1.csv"
-
-    Clonetype = inputcsv.split('/')[-1].split('.')[0]
-    
-    if 'nonclone' in inputcsv:
-        Clonetype = 'nonclone'
-    else:
-        Clonetype = 'clone'
-    
-    
-#     Clonetype = 'type-1'
+    source_root = "dataset/id2sourcecode/"
 
     pairs = pd.read_csv(inputcsv, header=None)
     pairs = pairs.drop(labels=0)
     pairs.columns = ['FunID1', 'FunID2']
-    
-    df_split = cut_df(pairs, 60)
-    
-    func1 = partial(get_sim, 't1')
-    '''
-    偏函数允许你固定一个或多个参数的值，从而生成一个新的可调用对象。
-    '''
-    pool = mp.Pool(processes=60)
-    sim_t1 = []
-    it_sim_t1 = tqdm.tqdm(pool.imap(func1, df_split))
-    '''
-    tqdm.tqdm()：创建一个进度条对象。
-    pool.imap()：从multiprocessing.Pool中调用，用于并行执行func1函数。imap会返回一个迭代器
-    func1：要并行执行的函数。
-    df_split：传递给func1的参数。
-    '''
-    for item in it_sim_t1:
-        sim_t1 = sim_t1 + item
-    pool.close()
-    pool.join()
 
-    func3 = partial(get_sim, 't3')
-    pool = mp.Pool(processes=60)
-    sim_t3 = []
-    it_sim_t3 = tqdm.tqdm(pool.imap(func3, df_split))
-    for item in it_sim_t3:
-        sim_t3 = sim_t3 + item
-    pool.close()
-    pool.join()
+    file_pairs = []
+    all_files = set()
 
-    func6 = partial(get_sim, 't6')
-    pool = mp.Pool(processes=60)
-    sim_t6 = []
-    it_sim_t6 = tqdm.tqdm(pool.imap(func6, df_split))
-    for item in it_sim_t6:
-        sim_t6 = sim_t6 + item
-    pool.close()
-    pool.join()
-    
-    func11 = partial(get_sim, 't11')
+    for _, row in pairs.iterrows():
+        f1 = os.path.join(source_root, str(row.FunID1) + ".java")
+        f2 = os.path.join(source_root, str(row.FunID2) + ".java")
 
-    pool = mp.Pool(processes=60)
-    sim_t11 = []
-    it_sim_t11 = tqdm.tqdm(pool.imap(func11, df_split))
+        file_pairs.append((f1, f2))
+        all_files.add(f1)
+        all_files.add(f2)
 
-    for item in it_sim_t11:
-        sim_t11 = sim_t11 + item
+    print("Training global LDA (Java)...")
+    topic_matrix, index = build_global_lda(list(all_files))
 
-    pool.close()
-    pool.join()
+    tasks = [(f1, f2, 1, topic_matrix, index) for f1, f2 in file_pairs]
 
+    with mp.Pool(mp.cpu_count()) as pool:
+        rows = pool.map(compute_pair, tasks)
 
-    result = pd.DataFrame({
-        'FunID1': pairs['FunID1'].to_list(),
-        'FunID2': pairs['FunID2'].to_list(),
-        't1_sim': sim_t1,
-        't3_sim': sim_t3,
-        't6_sim': sim_t6,
-        't9_sim': sim_t11
-    })
+    df = pd.DataFrame(rows, columns=[
+        "Jaccard", "Levenshtein",
+        "LDA", "LenRatio", "Label"
+    ])
 
-    result.to_csv(Clonetype + '_token_JaccardJaroLeven-RaLDA.csv', index=False)
-    
-    
+    df.to_csv(
+        "java_nonclone_features_global_lda.csv",
+        index=False
+    )
 
-if __name__ == '__main__':
-    parse_er_file = open('parser_error.txt', 'r', encoding='utf-8')  # 没有则创建
-    wrongfile = parse_er_file.read().split(' ')
-    #logfile = open('', 'a', encoding='utf-8')
+if __name__ == "__main__":
     start = time.time()
     main()
-
-    end = time.time()
-    t = end - start
-    print(t)
+    print("Total time:", time.time() - start)
